e4d33de12ccb423e1e04e1b254f822cf8fe64eae
lumsys <sunday4oged@yahoo.com>
2021-09-30 09:06:56 +0100

    authentication

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Carbon\Carbon;
+use App\User;
+
+
+class AuthController extends Controller
+{
+    //Register
+
+    public function register(Request $request){
+        $this->validate($request, [
+            'name' => 'required|min:3|max:50',
+            'email' => 'email',
+            'password' => 'required|confirmed|min:6',
+            'password_confirmation' => 'required|same:password',
+        ]);
+
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        $user->save();
+        return response()->json(['message' => 'user has been registered'], 200);       
+}
+
+//login function
+
+    public function login(Request $request)
+    {
+        $request-> validate([
+            'email' => 'required|string',
+            'password' => 'required|string',
+            'remember' => 'boolean'
+            
+        ]);
+
+        $login = request(['email', 'password']);
+
+        if(!Auth::attempt($login))
+        {
+            return response(['message'=> 'Invalid login credentials'], 401);
+        }
+
+        $user = $request->user();
+        $accessToken = $user->createToken('Personal Access Token');
+        $token = $accessToken->token;
+        $token ->expires_at = Carbon::now()->addWeeks(1);
+        $token->save();
+
+        return response()->json(['data'=>[
+            'user' => Auth::user(),
+            'access_token' => $accessToken->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse($accessToken->token->expires_at)->toDateTimeString()
+        ]]);
+    }
+    //logout function
+
+    public function logout() {
+
+        if(Auth::check()) {
+        Auth::user()->token()->revoke();
+        return response()->json(["status" => "success", "error" => false, "message" => "Success! You are logged out."], 200);
+        }
+        return response()->json(["status" => "failed", "error" => true, "message" => "Failed! You are already logged out."], 403);
+    }
+}
